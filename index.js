872bac7246f357a4068e97a9e697d8124147218d
YafimK <fkpkot@gmail.com>
2017-09-30 20:31:55 +0300

    added parsing tal dirctory to fetch roas

--- index.js
+++ index.js
@@ -1,27 +1,84 @@
-/**
- * Created by fimka on 30/09/2017.
- */
+"use strict";
 
 
 const rsync = require("rsyncwrapper");
+const fs = require('fs');
+let talDirPath = "./conf/tal/";
 
-const rsyncCB = function(error, stdout, stderr, cmd){
-    console.log("stdout: ",stdout);
-    console.log("cmd: ",cmd);
-    if(error){
-        console.error(error);
+function createTrustAnchor(file){
+    const lines = file.split('\n');
+    let values = [];
+    lines.forEach((line)=>{
+        let keyVal = line.split(' = ');
+        values.push(keyVal[1]);
+    });
+    return {
+        caName : values[0],
+        certificateLocation : values[1],
+        publicKey : values[2],
+        prefetchUris : values[3],
     }
-    if(stderr){
-        console.error(stderr);
-    }
-};
+}
 
-options = {
-    src: "rsync://rpki.apnic.net/member_repository/",
-    dest: ".//ROAS/",
-    recursive: true
-};
+function parseTrustAnchors(){
+    return new Promise((resolve, reject) => {
+        fs.readdir(talDirPath, (err, files) => {
+            if(err){
+                reject(err);
+            }  else {
+                resolve(files);
+            }
+        });
+    }).then((fileList) => {
+        let trustAnchors = [];
+        fileList.forEach((anchorFile)=>{
+            let file = '';
+            fs.createReadStream(talDirPath + anchorFile).on('data', (data)=>{
+                file += data;
+            }).on('end',()=>{
+                let trustAnchor = createTrustAnchor(file);
+                trustAnchors.push(trustAnchor);
+            }).on('error', function(err){
+                console.log(err);
+                reject(err);
+            });
+        });
+        resolve(trustAnchors);
+    }).catch((err)=>
+    console.log(err)).then(()=>{});
+}
+
+function fetchRoasByTrustAnchor(trustAnchor){
+    const suffixPos = trustAnchor.indexOf(".tal");
+    const anchorName = trustAnchor.substring(0, suffixPos);
+    const rsyncCB = function (error, stdout, stderr, cmd) {
+        console.log("stdout: ", stdout);
+        console.log("cmd: ", cmd);
+        if (error) {
+            console.error(error);
+        }
+        if (stderr) {
+            console.error(stderr);
+        }
+    };
+    const options = {
+        src: trustAnchor,
+        dest: ".//ROAS/"+anchorName,
+        recursive: true
+    };
+    rsync(options, rsyncCB);
+}
 
 
+function fetchRoas(){
+    let trustAnchors = parseTrustAnchors();
+    trustAnchors.then((tals)=>{
+        console.log(tals);
+        tals.forEach((trustAnchor)=>{
+            fetchRoasByTrustAnchor(trustAnchor.prefetchUris);
+        });
+    });
 
-rsync(options, rsyncCB);
+}
+
+fetchRoas();
